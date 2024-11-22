4b946754e548556674d6674246a334711ade8f75
MartinQR <quirozrmartin@gmail.com>
2024-11-21 20:35:06 -0600

    feat/probabilityCalculator

--- classes/diceParser.js
+++ classes/diceParser.js
@@ -1,19 +1,18 @@
-// diceParser.js
 class DiceParser {
   static parse(dices) {
-    
-    if (dices.length < 3) {
-        throw new Error("You must provide at least 3 dice configurations.");
-    }
-      
-    return dices.map(dice => {
-        const correctDice = dice.split(',').map(Number);
 
-        if (correctDice.length !== 6 || correctDice.some(isNaN)) {
-            throw new Error(`Invalid dice configuration: ${dice}. Each dice must have 6 integers.`);
-        }
-        
-        return correctDice;
+    if (dices.length < 3) {
+      throw new Error("You must provide at least 3 dice configurations.");
+    }
+
+    return dices.map(dice => {
+      const correctDice = dice.split(',').map(Number);
+
+      if (correctDice.length !== 6 || correctDice.some(isNaN)) {
+        throw new Error(`Invalid dice configuration: ${dice}. Each dice must have 6 integers.`);
+      }
+
+      return correctDice;
     });
   }
 }
